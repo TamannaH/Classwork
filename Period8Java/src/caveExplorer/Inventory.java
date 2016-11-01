2c0bb8d2db6322b2af105e76b9843e8121effd66
Student 8 <Student 8@10.8.33.169>
2016-11-01 14:04:16 -0400

    drawing

--- Period8Java/src/caveExplorer/Inventory.java
+++ Period8Java/src/caveExplorer/Inventory.java
@@ -2,4 +2,24 @@ package caveExplorer;
 
 public class Inventory {
 
+	private boolean hasMap;
+	private String map;
+	
+	public Inventory() {
+		hasMap = true;
+		updateMap();
+	}
+
+	private void updateMap() {
+		
+	}
+
+	public String getDescription() {
+		if(hasMap){
+			return map;
+		}else{
+			return "You have no inventory.";
+		}
+	}
+
 }
--- Period8Java/src/caveExplorer/TwoDArraysIntro.java
+++ Period8Java/src/caveExplorer/TwoDArraysIntro.java
@@ -9,11 +9,13 @@ public class TwoDArraysIntro {
 		System.out.println(Arrays.toString(xox));
 		//a 1D array prints a horizontal string
 		
-		String[][] arr2D = new String[5][4];
+		//String[][] arr2D = new String[4][4];
+		
+		//String[][] arr2D = new String[5][4];
 		//System.out.println("The height is " + arr2D.length);
 		//System.out.println("The width is " + arr2D[0].length);
 		//5 is the row and the column is 4
-		
+		/*
 		for(int row = 0; row < arr2D.length; row++){
 			//populate with coordinates
 			for(int col = 0; col < arr2D[row].length; col++){
@@ -25,6 +27,40 @@ public class TwoDArraysIntro {
 		//so a for-each loop looks like this:
 		for(String[] row : arr2D){
 			System.out.println(Arrays.toString(row));
+		}*/
+		
+		CaveRoom[][] caves = CaveExplorer.caves;
+		//convert these caves to a string representation
+		
+		String[][] pic = new String[10][8];
+		
+		for (int row = 0; row < pic.length; row++){
+			for(int col = 0; col < pic[row].length; col++){
+				pic[row][col] = " ";
+			}
 		}
+		
+		//sun
+		pic[0][3] = "|";
+		pic[1][3] = "O";
+		pic[2][3] = "|";
+		pic[1][2] = "-";
+		pic[1][4] = "-";
+		
+		//grassy field
+		for(int row = 5; row < pic.length; row++){
+			for(int col = 0; col < pic[row].length; col++){
+				pic[row][col] = "W";
+			}
+		}
+		
+		for (String[] row : pic){
+			for (String col: row){
+				System.out.print(col);
+			}
+			System.out.println();
+		}
+		
+		
 	}
 }

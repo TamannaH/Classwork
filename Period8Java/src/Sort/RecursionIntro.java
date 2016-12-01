17b807c3d5005844282fcd2143d50ab2e5acf8f7
Student 8 <Student 8@10.8.33.169>
2016-12-01 14:02:29 -0500

    Fibonacci

--- Period8Java/src/Sort/RecursionIntro.java
+++ Period8Java/src/Sort/RecursionIntro.java
@@ -3,8 +3,9 @@ package Sort;
 public class RecursionIntro {
 
 	public static void main(String[] args) {
-		
-		hanoiSolution(14, "A", "B", "C");
+		int n = 5;
+		System.out.println("The " + n +"th Fibonacci is " + fibonacci(n));
+		//hanoiSolution(3, "A", "B", "C");
 		
 //		System.out.println("Using a for loop.");
 //		for(int i = 0; i < 5; i++){
@@ -21,6 +22,18 @@ public class RecursionIntro {
 //		});
 	}
 	
+	private static int fibonacci(int n) {
+		if (n <= 1){
+			return 1;
+		}else{
+			int previous = fibonacci(n-1);
+			print("Before we find fibonacci "+ n +" we need to find fibonacci " + (n-1) + ", which"
+					+ " is " + "previous");
+			int beforePrevious = fibonacci(n-2);
+			return previous + beforePrevious; 
+		}
+	}
+
 	public static int factorial(int x){
 		//for loop method
 		int factorial = 1;
@@ -51,10 +64,19 @@ public class RecursionIntro {
 		}
 	}
 	
+	private static int count = 1;
+	
+	public static void print(String s){
+		System.out.println("Move #" + count + ":" + s);
+		count++;
+	}
+	
 	public static void hanoiSolution(int numberOfDiscs, String startPeg, String midPeg, String endPeg){
 		if(numberOfDiscs <= 1){
-			System.out.println("Move " + startPeg + " to " + endPeg);
+			print("Move " + startPeg + " to " + endPeg);
 		}else{
+			System.out.println("In order to move " + numberOfDiscs + " over to peg " + endPeg + " we must move "
+					+ (numberOfDiscs - 1) + " over to peg " + midPeg + " first.");
 			hanoiSolution(numberOfDiscs-1,startPeg, endPeg, midPeg);
 			hanoiSolution(1, startPeg, midPeg, endPeg);
 			hanoiSolution(numberOfDiscs-1, midPeg, startPeg, endPeg);
